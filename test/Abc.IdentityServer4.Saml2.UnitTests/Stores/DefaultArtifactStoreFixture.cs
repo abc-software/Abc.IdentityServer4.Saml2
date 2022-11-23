dae935677d113de971be8bdcd0492f245da7ffbb
Juris Gekiss <gekiss@inbox.lv>
2022-11-23 11:14:28 +0200

    Update libraries

--- test/Abc.IdentityServer4.Saml2.UnitTests/Stores/DefaultArtifactStoreFixture.cs
+++ test/Abc.IdentityServer4.Saml2.UnitTests/Stores/DefaultArtifactStoreFixture.cs
@@ -6,7 +6,7 @@ using System;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace Abc.IdentityServer4.Saml2.UnitTests.Stores
+namespace Abc.IdentityServer4.Saml2.Stores.UnitTests
 {
     public class DefaultArtifactStoreFixture
     {
